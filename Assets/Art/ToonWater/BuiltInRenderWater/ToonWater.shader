9b2d47ab80d56dcd51ff21789c48168c6fcf6306
Ruslan Kartechev <kartechevruslan@gmail.com>
2023-05-17 13:48:02 +0600

    DEV art chage

---
+++ Assets/Art/ToonWater/BuiltInRenderWater/ToonWater.shader
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2012ef51ab37407c4fe30f5a05446c72cd419af1f07fc4df9aab898c10067ee3
+size 15243
--- Assets/Scripts/CollectShooter/Player/PlayerBehaviour.cs
+++ Assets/Scripts/CollectShooter/Player/PlayerBehaviour.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1bf79e6c6fb37e57270f3bbfe0f046de46f53c05c65c7c4cac136be3192eba3b
-size 5055
+oid sha256:3f012a49c422c9a84f87b6af8c827dbff8a2c22b3c40c8b9a16fa222923d605e
+size 5120
